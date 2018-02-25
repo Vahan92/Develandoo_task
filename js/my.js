e8f280d51b0161b2274953f520f8d9eacc4d8583
Vahan92 <vahan.kalenteryan@gmail.com>
2018-02-25 11:49:32 +0400

    fixed little bug

--- js/my.js
+++ js/my.js
@@ -1,22 +1,37 @@
-let myApp = function() {
-    let max = 100,
-        min = 4,
-        print = document.getElementById("print")
-    return {
+var MyApp = function() {
+    let self = this,
+        max = 100,
+        min = 4,        
+        print = document.getElementById("print");
 
-        getId: function(params) {
+        init = function (divId) {
+            forCells = divId;
+        },
+
+        getId = function(params) {
             return document.getElementById(params);
         },
 
-        printing: function(inpVal) {
+        printing= function(inpVal) {
             if (typeof print !== null) {
-                print.textContent = "Number of cells: " + inpVal.value;
+                print.textContent = "Number of cells: " + inpVal.value * inpVal.value;
             }
         },
 
-        draw: function() {
-            let inputVal = myApp.getId("num").value;
-            let forCells = myApp.getId("forCells");
+        checkingBtn = function() {
+            if(forCells.firstChild){
+                self.getId('resets').disabled = false;
+                self.getId('colourises').disabled = false;
+                self.getId('draws').disabled = true;
+            } else {
+                self.getId('resets').disabled = true;
+                self.getId('colourises').disabled = true;
+                self.getId('draws').disabled = false;
+            }
+        },
+
+        draw= function() {
+            let inputVal = self.getId("num").value;
             if (inputVal < min || inputVal > max) {
                 alert(`Please enter a number between ${min} and ${max}, or you are going to get this annoying alert notification :)`);
             } else {
@@ -38,40 +53,48 @@ let myApp = function() {
                     fragment.appendChild(newCol);
                 }
                
-                myApp.getId('resets').disabled = false;
-                myApp.getId('colourises').disabled = false;
-                myApp.getId('draws').disabled = true;
+                // MyApp.get('resets').disabled = false;
+                // MyApp.get('colourises').disabled = false;
+                // MyApp.get('draws').disabled = true;
                 forCells.appendChild(fragment);
+                checkingBtn();
             }
+        },       
+
+        getColor=  function() {
+            var r = function () { return Math.floor(Math.random()*256) };
+            return "rgb(" + r() + "," + r() + "," + r() + ")";
         },
 
-        getRandomColor: function() {
-            let letters = '0123456789ABCDEF';
-            let color = '#';
-            for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        },
-
-        colourise: function() {
+        colourise=  function() {
             let toColour =  document.querySelectorAll('.column > div'); 
             for (let i = 0; i < toColour.length; i++) {
                     setTimeout(function() {
-                        toColour[i].style.backgroundColor = myApp.getRandomColor();
+                        toColour[i].style.backgroundColor = self.getColor();
                         toColour[i].style.transition = "all 1.5s";
                     }, 1500 * i);           
         }
         },
 
-        reset: function() {
+        reset=  function() {
             print.textContent = "";
             while (forCells.firstChild) {
                 forCells.removeChild(forCells.firstChild);
             }
-            myApp.getId('resets').disabled = true;
-            myApp.getId('colourises').disabled = true;
-            myApp.getId('draws').disabled = false;
+            checkingBtn();
         }
+
+    return {
+        init: init,
+        printing: printing,
+        draw: draw,
+        colourise: colourise,
+        reset: reset
     }
-}();
+}
+
+var simpleApp;
+
+    var divId = document.getElementById('forCells');
+    simpleApp = MyApp(); //Invoke the object (function assigned to the object) and assign to simpleApp
+    simpleApp.init(divId);
